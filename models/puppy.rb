dc9e0dfd1dc970e6b50d7a1e7df122c054ffd914
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-07 04:21:51 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,19 +3,9 @@
 class Puppy
   attr_accessor :name, :breed, :age
   
-  @@all = []
-  
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-  end
-  
-  def save
-    @@all << self
-  end
-  
-  def self.all
-    @@all
   end
 end
